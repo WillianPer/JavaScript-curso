3f379126e4eba38963441f8f089ab344cc5947d2
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-02 15:25:17 -0400

    criando arquivo de exemplo de comentarios

---
+++ comentariosDeCodigo.js
@@ -0,0 +1,11 @@
+//Exemplos de comentarios
+
+//1º
+// console.log é usado para exibir algo na tela do computador!
+
+//2º
+// console.log("Bom dia!");
+
+//3º
+/*console.log("Boa tarde!");
+console.log("Boa noite!");*/
