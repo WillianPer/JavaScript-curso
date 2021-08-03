bbc812d5eabb93cdb5e8961d2d9432a9af3cd4a7
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-03 13:57:46 -0400

    criando 3º arquivo de manipulação de dados e tipagem de variáveis

---
+++ dados_3.js
@@ -0,0 +1,12 @@
+let nome = "wilian";
+console.log(typeof nome);
+
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
