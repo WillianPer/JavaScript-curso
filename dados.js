da1dc2c18358da6b9d97b51a06425eff1f1d7898
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-03 09:24:36 -0400

    criando arquivo de manipulação de dados

---
+++ dados.js
@@ -0,0 +1,11 @@
+var nome = "caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log("produto:" + nome);
+console.log("Quantidade:" + quantidade);
+console.log("preco:" + preco);
+console.log("imposto:" + imposto);
+console.log("preço final:" + precoFinal);
