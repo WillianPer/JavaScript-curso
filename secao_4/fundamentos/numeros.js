7acadbdd282b1256fa4f4b88f42e1de9e79f4bb2
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-04 16:11:08 -0400

    apresentando exemplos de dados numérico

--- secao_4/fundamentos/numeros.js
+++ secao_4/fundamentos/numeros.js
@@ -11,5 +11,5 @@ const avaliacao2 = 6.871
 const total = avaliacao * peso1 + avaliacao2 * peso2
 const media = total / (peso1 + peso2)
 console.log(media.toFixed(2))
-console.log(media.toString(2))
+console.log(media.toString(2)) // apresenta valor em binario
 console.log(typeof media)
