82be60026170bd0329566a6dba6adf24fcaafefb
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-05 09:49:06 -0400

    introdução a objeto

---
+++ secao_4/fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = "Celular ultra mega"
+prod1.preco = 4392.57
+prod1 ['Super desconto'] = 0.40 // Evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome = 'camisa polo',
+    preco = 79.90
+}
+
+console.log(prod2)
