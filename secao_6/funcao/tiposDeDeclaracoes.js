47036faa5b35e639471aea8ea4bfe72279469c92
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-12 19:38:54 -0400

    Introdução sobre tipos de declarações de funções

---
+++ secao_6/funcao/tiposDeDeclaracoes.js
@@ -0,0 +1,25 @@
+console.log(soma(2, 3))
+
+// function declaration
+function soma(a, b) {
+    return a + b
+}
+
+// function arrow
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(4, 5))
+
+// function expression
+const divisao = function (a, b) {
+    return a / b
+}
+
+console.log(divisao(4, 2))
+
+// named function expression
+const multiplicacao = function multiplicacao (a, b) {
+    return a * b
+}
+
+console.log(multiplicacao(4, 5))
