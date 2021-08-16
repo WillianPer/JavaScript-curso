7fbe5cb80bf41ad3556714b1615bd96005c9fd01
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-15 22:34:18 -0400

    Introdução a Herança Aula 6

---
+++ secao_7/objeto/heranca_6.js
@@ -0,0 +1,20 @@
+function Aula (nome, videoID) {
+    this.nome = nome
+    this.videoID = videoID
+}
+
+const aula1 = new Aula("Bem vindos", 123)
+const aula2 = new Aula("Até mais", 456)
+console.log(aula1, aula2)
+
+// simulando Operador new
+/*function novo (f, ...params) {
+    const obj = {}
+    obj.__proto__ = f.prototype
+    f.apply(obj, params)
+    return obj
+}*/
+
+const aula3 = novo("Bem vindo", 123)
+const aula4 = novo("Até mais", 456)
+console.log(aula3, aula4)
