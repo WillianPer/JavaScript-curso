ca4a63cb10625e48eb4b4a8b11838b2c83ec2a28
wilianPer <wsresilienttasteshopw@gmail.com>
2021-08-19 19:13:27 -0400

    Introdução ao método concat

---
+++ secao_8/array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ["camila", "carol"]
+const filhos = ["thiago", "kenan"]
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
